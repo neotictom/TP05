7259750b15b02227a6bdbde10f07596712b39e9f
TOMAS NEIMERMAN GROSSO <47496739@ort>
2023-06-12 09:00:47 -0300

    6

--- Models/Escape.cs
+++ Models/Escape.cs
@@ -2,7 +2,7 @@ class Escape{
     public static string[] IncognitasSalas{get; private set;} = new string [4] {"La oscuridad","El segundo","El aleman",""};
     public static int estadoJuego{get; private set;}
     private static void InicializarJuego(){
-
+        
     }
     public static int GetEstadoJuego(){
         int i = 0;
